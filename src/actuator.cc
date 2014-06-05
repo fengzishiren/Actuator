8a21852fba0b996da5ae9c52bb6523b5e0465eb0
Lunatic Zheng <lunaticzheng@gmail.com>
2014-06-06 00:14:38 +0800

    增加基本求值运算

--- src/actuator.cc
+++ src/actuator.cc
@@ -4,6 +4,7 @@
  *      Author: Lunatic
  */
 
+#include <cstring>
 #include "actuator.h"
 #include "lexer.h"
 #include "alarm.h"
@@ -70,7 +71,7 @@ void Actuator::load(const std::string& script_code) {
 					inst.params.push_back(tokens[i]);
 					break;
 				default:
-					error("语法错误 参数不符合要求！", tokens[0].pos);
+					error("参数不符合要求！", tokens[0].pos);
 				}
 			}
 		}
@@ -84,30 +85,108 @@ void Actuator::load(const std::string& script_code) {
 
 	}
 }
-
+/*
+ * 如果是变量 存在则直接取出 否则报错 ？不太妥当
+ * 其他直接返回名字
+ *
+ */
 std::string get_val_or_var(Env& env, Token& token) {
 	if (token.type == kName) { //处理变量
 		if (env.contains(token.token)) {
 			return env.get(token.token);
 		} else
-			error("语法错误 变量未定义" + token.token, token.pos);
+			error("变量未定义" + token.token, token.pos);//？不太妥当
 	}
 	return token.token;
 }
 
+bool is_real(const std::string& number) {
+	return std::string::npos != number.find('.');
+}
+
+bool is_int(const std::string& number) {
+	return !is_real(number);
+}
+
+long long str2int(const std::string& val) {
+	//char *err = NULL;
+	//long long retval = std::strtoll(val.c_str(), &err, 10);
+	//if (*err == '\0')
+	//return retval;
+
+	//忽略错误
+	return std::strtoll(val.c_str(), NULL, 10);
+}
+
+long double str2double(const std::string& val) {
+	//char *err = NULL;
+	//long double retval = std::strtold(val.c_str(), &err);
+	//if (*err == '\0')
+		//return retval;
+	//else
+		//
+		//error!
+		//return 0.0;
+	return std::strtold(val.c_str(), NULL);
+
+}
+//3.3333
+
+bool is_zero(const std::string& vfloat) {
+	bool left = true, right = true;
+	size_t start = 0, pos = vfloat.find('.');
+
+	if (pos == std::string::npos)
+		pos = vfloat.size();
+
+	while(start < pos) {
+		if(vfloat[start++] != '0')
+		{
+			left = false;
+		}
+	}
+	while(++pos < vfloat.size()) {
+		if(vfloat[pos] != '0')
+		{
+			right = false;
+		}
+	}
+	return left && right;
+
+}
+
 std::string eval(const std::string& cmd, const std::string& arg1,
 		const std::string& arg2) {
+
+#define EVAL(RES, X, Y, OP)                          \
+	do {                                             \
+		if(is_real(X) || is_real(Y)) {               \
+			RES << (str2double(X) OP str2double(Y)); \
+		} else {                                     \
+			RES << (str2int(X) OP str2int(Y));       \
+		}                                            \
+	}while(0)                                        \
+
+	std::stringstream ss;
 	if (cmd == "add") { // +
-
+		EVAL(ss, arg1, arg2, +);
 	} else if (cmd == "min") { //-
-
+		EVAL(ss, arg1, arg2, -);
 	} else if (cmd == "mul") { // *
-
+		EVAL(ss, arg1, arg2, *);
 	} else if (cmd == "div") { // /
-
+		if (is_zero(arg2)) {
+			error("除数不能为0！");
+		}
+		EVAL(ss, arg1, arg2, /);
 	}
-	return std::string();
+	return ss.str();
+
+#undef EVAL
 }
+
+
+
 /*
  *
  * 依次处理指令
@@ -125,7 +204,7 @@ void Actuator::run(Env& env) {
 			if (pc.params.empty())
 				break;
 			else
-				error("语法错误 exit不能有参数", pc.pos);
+				error("exit不能有参数", pc.pos);
 		} else if (pc.name == "goto") {
 			if (pc.params.size() == 1 && pc.params[0].type == kName) {
 				//处理指令跳转
@@ -149,14 +228,19 @@ void Actuator::run(Env& env) {
 				|| pc.name == "div") {
 			//add a b 1 => a = b + 1
 			if (pc.params.size() == 3 && pc.params[0].type == kName) {
+				//把param1和param2求值 把结果放入param0为key结果为value的环境map中
 				std::string arg1 = get_val_or_var(env, pc.params[1]);
 				std::string arg2 = get_val_or_var(env, pc.params[2]);
 				env.put(pc.params[0].token, eval(pc.name, arg1, arg2));
 			} else {
 				error(pc.name + "命令需要一个变量和两个参数", pc.pos);
 			}
-		} else if(pc.name == "if") { //if
+		} else if(pc.name == "if") { //if value1 [opcode value2] goto label
 
+			if(pc.params.size() == 3) { //if val goto label
+				std::string val = get_val_or_var(env, pc.params[0]);
+
+			}
 		}
 
 		///////////////////////////////////////////////////
